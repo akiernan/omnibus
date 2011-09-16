78b6b6e83e527c06abecedc640367f6abcf46a85
Alex Kiernan <alexk@alexandalex.com>
2011-09-16 10:27:53 +0100

    Put variable couchdb data outside of the full stack directory so we don't blow it away on upgrades

--- config/software/couchdb.clj
+++ config/software/couchdb.clj
@@ -25,11 +25,21 @@
                     {:env env
                      :command "./configure"
                      :args ["--prefix=/opt/opscode/embedded"
-                            "--disable-init"
+                            "--sysconfdir=/etc"
+                            "--localstatedir=/var"
                             "--disable-launchd"
                             "--with-erlang=/opt/opscode/embedded/lib/erlang/usr/include"
                             "--with-js-include=/opt/opscode/embedded/include"
                             "--with-js-lib=/opt/opscode/embedded/lib" ]}
                     {:env env :command "make"}
-                    {:env env :command "make" :args ["install"]} ]))
-
+                    {:env env
+                     :command "make"
+                     :args ["DESTDIR=/opt/opscode/root"
+                            "install"]}
+                    {:command "/opt/opscode/embedded/bin/rsync"
+                     :args ["-a"
+                            "/opt/opscode/root/opt/opscode/"
+                            "/opt/opscode"]}
+                    {:command "rm"
+                     :args ["-fR"
+                            "/opt/opscode/root/opt/opscode"]} ]))
